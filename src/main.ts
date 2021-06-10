e29c3ad4fb93877480d353154f2ae29033d3fd8c
Eliad Tuksher <eliad91@gmail.com>
2021-06-10 09:41:04 +0300

    adding semantic header and basic styles

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import './style.scss';
+import './main.scss';
 import { HttpResult } from './interfaces';
 
 const app = document.querySelector<HTMLDivElement>('#app')
@@ -12,8 +12,4 @@ if(app) {
     
     
   })()
-  app.innerHTML = `
-    <h1>Hello Vite!!!!</h1>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-  `
 }
