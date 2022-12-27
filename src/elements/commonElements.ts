8b986bc3ac4b2818baef797eac7ddb18a9071c1b
Eliad Tuksher <eliad91@gmail.com>
2022-12-27 11:52:22 +0200

    Revisit code and minor refactors

--- src/elements/commonElements.ts
+++ src/elements/commonElements.ts
@@ -1,34 +1,33 @@
-import {
-    ChildrenType,
-    PropOptions
-} from '../interfaces';
+import { ChildrenType, PropOptions } from '../interfaces';
 import generateElement from './generateElement';
 
 /**
  * a constant list of HTML element types to generate with the generateElement function.
  * To generate more element types, simply add the type (tag name) as a string to the list.
  */
-const ELEMENT_TYPES_LIST = ['header', 'section', 'article', 'span', 'div', 'img', 'h2', 'a', 'p'] as Array<keyof HTMLElementTagNameMap>;
+const ELEMENT_TYPES_LIST: Readonly<keyof HTMLElementTagNameMap>[] = [
+	'header',
+	'section',
+	'article',
+	'span',
+	'div',
+	'img',
+	'h2',
+	'a',
+	'p',
+];
 
 /**
- * Map over the element type list and with the help of some closure magic to pass around arguments, 
- * generateElement function, on each to create a callable function with a preset element type, 
- * that can also accept other element functions as children. 
+ * Map over the element type list and with the help of some closure magic to pass around arguments,
+ * generateElement function, on each to create a callable function with a preset element type,
+ * that can also accept other element functions as children.
  * Using destructuring, assign each to a variable by the same name, and make sure they're in the same order.
  */
-const [header, section, article, span, div, img, h2, a, p] = ELEMENT_TYPES_LIST.map(element =>
-    (propOptions: PropOptions, ...childElements: ChildrenType) =>
-    generateElement(element , propOptions, ...childElements))
+const [header, section, article, span, div, img, h2, a, p] = ELEMENT_TYPES_LIST.map(
+	(element) =>
+		(propOptions: PropOptions, ...childElements: ChildrenType) =>
+			generateElement(element, propOptions, ...childElements)
+);
 
 // Don't forget to export any newly added elements!
-export {
-    section,
-    article,
-    header,
-    span,
-    div,
-    img,
-    h2,
-    a,
-    p
-};
+export { section, article, header, span, div, img, h2, a, p };
--- src/main.ts
+++ src/main.ts
@@ -1,52 +1,46 @@
-import {
-  HttpResult,
-  UrlParams
-} from './interfaces';
+import { HttpResult, UrlParams } from './interfaces';
 import buildUrl from './utils/buildUrl';
-import {
-  generateRecommendationItemByOrigin,
-  generateWidgetSectionByOrigin
-} from './elements/widgetContentElements';
+import { generateRecommendationItemByOrigin, generateWidgetSectionByOrigin } from './elements/widgetContentElements';
 import './elements/element-styles.scss';
 import './main.scss';
 
-const app = document.querySelector < HTMLDivElement > ('#app');
+const app = document.querySelector<HTMLDivElement>('#app');
 const urlParams: UrlParams = {
-  apiKey: 'f9040ab1b9c802857aa783c469d0e0ff7e7366e4',
-  appType: 'desktop',
-  publisherId: 'taboola-templates',
-  sourceId: '214321562187'
-}
+	apiKey: 'f9040ab1b9c802857aa783c469d0e0ff7e7366e4',
+	appType: 'desktop',
+	publisherId: 'taboola-templates',
+	sourceId: '214321562187',
+};
 
 if (app) {
-  // init after the DOM content has loaded
-  document.addEventListener('DOMContentLoaded', async () => {
-    const url = buildUrl(urlParams);
-    try {
-      const res: Promise<HttpResult> = (await fetch(url)).json();
-      const { list } = await res;
+	// init after the DOM content has loaded
+	document.addEventListener('DOMContentLoaded', async () => {
+		const url = buildUrl(urlParams);
+		try {
+			const res: Promise<HttpResult> = (await fetch(url)).json();
+			const { list } = await res;
 
-      for (const data of list) {
-        // first check if a section with the designated origin value already exists, if so use it
-        let widgetOriginSection = document.querySelector<HTMLElement>(`[data-origin=${data.origin}]`);
+			for (const data of list) {
+				// first check if a section with the designated origin value already exists, if so use it
+				let widgetOriginSection = document.querySelector<HTMLElement>(`[data-origin=${data.origin}]`);
 
-        // if not, create a new section element for the new origin and use it
-        if (!widgetOriginSection) {
-          widgetOriginSection = generateWidgetSectionByOrigin(data.origin);
+				// if not, create a new section element for the new origin and use it
+				if (!widgetOriginSection) {
+					widgetOriginSection = generateWidgetSectionByOrigin(data.origin);
 
-          app.append(widgetOriginSection);
-        }
+					app.append(widgetOriginSection);
+				}
 
-        // get the widget content container of the widget's section element
-        const contentContainer = widgetOriginSection.querySelector<HTMLElement>('.widget-content-container')!;
+				// get the widget content container of the widget's section element
+				const contentContainer = widgetOriginSection.querySelector<HTMLElement>('.widget-content-container')!;
 
-        // generate the content item node with the publisher's data and append it to the content container
-        const publisherContentItem = generateRecommendationItemByOrigin(data);
+				// generate the content item node with the publisher's data and append it to the content container
+				const publisherContentItem = generateRecommendationItemByOrigin(data);
 
-        if (publisherContentItem) contentContainer.append(publisherContentItem);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  });
+				if (publisherContentItem) contentContainer.append(publisherContentItem);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	});
 }
